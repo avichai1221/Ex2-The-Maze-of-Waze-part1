5a7a33a03fa7babce0df22c737d17a31e45ef7f3
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2019-12-25 01:23:55 +0200

    Add files via upload
    start DGraph

--- src/algorithms/Graph_Algo.java
+++ src/algorithms/Graph_Algo.java
@@ -1,7 +1,14 @@
 package algorithms;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import dataStructure.DGraph;
+import dataStructure.NodeData;
+import dataStructure.edge_data;
 import dataStructure.graph;
 import dataStructure.node_data;
 /**
@@ -12,12 +19,44 @@ import dataStructure.node_data;
  */
 public class Graph_Algo implements graph_algorithms{
 
+	
+	private HashMap<Integer,node_data> nodeCol=new HashMap<Integer,node_data>();
+	private HashMap<Integer,edge_data> edgeCol=new HashMap<Integer,edge_data>();
+	//private HashMap<Integer,HashMap<Integer,edge_data>> edgeCol=new HashMap<Integer,HashMap<Integer,edge_data>>();
 	@Override
 	public void init(graph g) {
-		// TODO Auto-generated method stub
 		
+		HashMap<Integer,HashMap<Integer,edge_data>> help3=new HashMap<Integer,HashMap<Integer,edge_data>>();
+		for(node_data n: g.getV()) {
+			int k=n.getKey(); 
+			HashMap<Integer,node_data> help=new HashMap<Integer,node_data>();
+			help.put(k, n);
+			
+			if(g.getNode(k)!=null) {
+			for (edge_data e: g.getE(k)) {
+				if(edgeCol.containsKey(k)) {
+					
+				}
+				HashMap<Integer,edge_data> help1=new HashMap<Integer,edge_data>();
+				help1.put(k, e);
+				help3.put(k, help1);
+			}
+			}
+		
+			DGraph d=new DGraph(help,help3);
+		}
+	
+		
+		
+		
+		
+		
+		
+	
+	       
 	}
-
+	
+	
 	@Override
 	public void init(String file_name) {
 		// TODO Auto-generated method stub
